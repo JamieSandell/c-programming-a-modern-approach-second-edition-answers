104d0cd80cb6653e449e51389ca95ff933a9d2a2
jamiesandell <jamie.sandell@outlook.com>
2021-10-29 15:02:50 +0000

    Chapter 02 - Exercise 05

---
+++ Chapter 02/Exercises/05/main.c
@@ -0,0 +1,11 @@
+/*
+Which of the following are not legal C identifiers?
+
+(a) 100_bottles
+(b) _100_bottles
+(c) one__hundred__bottles
+(d) bottles_by_the_hundred_
+
+(a) is not a valid C identifier.
+Whilst (b) is a valid C identifier, “…predefined macro names shall begin with a leading underscore followed by an uppercase letter or a second underscore.”
+*/
