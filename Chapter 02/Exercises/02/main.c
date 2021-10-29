8d3a3ccdebb8b94052ddad677e31c9322599abaf
jamiesandell <jamie.sandell@outlook.com>
2021-10-29 14:36:17 +0000

    Chapter 02 - Exercise 02

---
+++ Chapter 02/Exercises/02/main.c
@@ -0,0 +1,18 @@
+/*
+Consider the following program:
+#include <stdio.h>
+
+int main(void)
+{
+    printf("Parkinson's law:\nWork expands so as to ");
+    printf("fill the time\n");
+    printf("available for its completion\n");
+    return 0;
+}
+
+It prints:
+Parkinson's law
+Work expands so as to fille the time
+available for its completion
+
+*/
