f744fcb47fa5240eb2cb976895389a3c5cb8d4a9
jamiesandell <jamie.sandell@outlook.com>
2021-10-31 07:15:46 +0000

    Chapter 02 - Exercise 07

---
+++ Chapter 02/Exercises/07/main.c
@@ -0,0 +1,8 @@
+/*
+Which of the following are keywords in C?
+(a) for         - keyword
+(b) If          - not a keyword
+(c) main        - not a keyword
+(d) printf      - not a keyword
+(c) while       - keyword
+*/
