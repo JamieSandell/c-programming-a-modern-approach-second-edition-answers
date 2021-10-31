f91459a235f181dcb684a116343ad244ec2f9f23
jamiesandell <jamie.sandell@outlook.com>
2021-10-31 07:25:50 +0000

    Chapter 02 - Exercise 08

---
+++ Chapter 02/Exercises/08/main.c
@@ -0,0 +1,24 @@
+/*
+How many tokens are there in the following statement?
+answer=(3*q-p*q)/3;
+
+In C a token is broken down into one of the following:
+Keywords
+Operators
+Strings
+Constants
+Special Characters
+Identifiers
+
+The tokens (10 of them) are:
+= (special character)
+( (special character)
+3 (integer constant)
+* (operator)
+- (operator)
+* (operator)
+) (special character)
+/ (operator)
+3 (integer constant)
+; (special characters)
+*/
