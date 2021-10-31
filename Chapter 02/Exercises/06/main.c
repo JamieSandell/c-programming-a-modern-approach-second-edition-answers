f92c68b1176eae650463fa3dd64a64266b8ee331
jamiesandell <jamie.sandell@outlook.com>
2021-10-31 07:12:55 +0000

    Chapter 02 - Exercise 06

---
+++ Chapter 02/Exercises/06/main.c
@@ -0,0 +1,6 @@
+/*
+Why is it not a good idea for an identifier to contain more than one adjacent underscore
+(as in current___balance)?
+
+Hard to tell how many underscores there are / hard to read.
+*/
