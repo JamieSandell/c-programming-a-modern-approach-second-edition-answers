fca83c2cc95abc85db0c2ef8ae52b1cc09af7e29
JamieSandell <jamie.sandell@outlook.com>
2022-05-16 21:15:15 +0100

    Chapter 2 - Exercise 10

---
+++ Chapter 02/Exercises/10/main.c
@@ -0,0 +1,7 @@
+/*
+In the dweight.c program (Section 2.4) which spaces are essential?
+
+The ones between #include and the header file.
+The one between int and main.
+The ones between the type identifiers and the variables.
+*/
