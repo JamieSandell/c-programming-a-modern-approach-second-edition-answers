ed3bd405d27e784de43ced96965c9ce99f3b640f
JamieSandell <jamie.sandell@outlook.com>
2022-05-16 21:39:34 +0100

    Chapter 2 - Project 1

---
+++ Chapter 02/Projects/01/main.c
@@ -0,0 +1,23 @@
+/*
+Write a program that uses printf to display the following picture on screen:
+            *
+           *
+          *
+     *   *
+      * *
+       *
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("            *\n");
+    printf("           *\n");
+    printf("          *\n");
+    printf("     *   *\n");
+    printf("      * *\n");
+    printf("       *\n");
+    return EXIT_SUCCESS;
+}
