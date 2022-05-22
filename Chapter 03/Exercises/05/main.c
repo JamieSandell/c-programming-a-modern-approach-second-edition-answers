963f5ba6d73f006daee3e533d07871f19c789c6a
JamieSandell <jamie.sandell@outlook.com>
2022-05-22 21:11:28 +0100

    Chapter 3 - Exercise 5

---
+++ Chapter 03/Exercises/05/main.c
@@ -0,0 +1,15 @@
+/*
+Suppose that we call scanf as follows: 
+
+n = scanf("%d%f%d", &i, &x, &j); 
+
+(i. j. and n are int variables and x is a float variable.) Assuming that the input stream 
+contains the characters shown, give the values of i, j. n. and x after the call. In addition, 
+indicate which characters were consumed by the call. 
+
+(a) 10*20*30D 
+
+(b) 1.0*2.0*3.00 
+(C) 0.1*0.2*0.30 
+(d) . 1* .2* .30 
+*/
