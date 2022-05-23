219e20bb27a07be8a36d8fba3a2548fc0d4d0671
Jamie Sandell <jamie.sandell@outlook.com>
2022-05-23 08:29:17 +0100

    Chapter 3 - Exercise 5

--- Chapter 03/Exercises/05/main.c
+++ Chapter 03/Exercises/05/main.c
@@ -8,8 +8,34 @@ contains the characters shown, give the values of i, j. n. and x after the call.
 indicate which characters were consumed by the call. 
 
 (a) 10*20*30D 
+i = 10, n = 1, 1 and 0 consumed by the call.
+x = x, n = 0, * consumed by the call.
+j = j, n = 0, * consumed by the call.
 
 (b) 1.0*2.0*3.00 
+i = 1, n = 1, 1 consumed by the call.
+x = 0.1, n = 2, . and 1 consumed by the call.
+j = j, n = 0, no characters consumed.
+
 (C) 0.1*0.2*0.30 
 (d) . 1* .2* .30 
 */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int n;
+    int i;
+    float x;
+    int j;
+
+    n = scanf("%d%f%d", &i, &x, &j);
+    printf("i = %d, n = %d, 1 and 0 consumed by the call.\n", i, n);
+    printf("x = %f, n = %d, * consumed by the call.\n", x, n);
+    printf("j = %d, n = %d, * consumed by the call.\n", j, n);
+
+
+    return EXIT_SUCCESS;
+}
