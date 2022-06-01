b2803ffaf17944e6f5325771690aee3aed514622
JamieSandell <jamie.sandell@outlook.com>
2022-06-01 21:59:57 +0100

    Chapter 4 - Exercise 10

--- Chapter 04/Exercises/10/main.c
+++ Chapter 04/Exercises/10/main.c
@@ -6,20 +6,25 @@ and k are int variables.
     printf("%d ", i++ - 1);
     printf("%d", i);
 
-    0
-    1
+    0 2
 
 (b) i = 10; j = 5;
     printf("%d ", i++ - ++j);
     printf("%d %d", i, j);
 
+    4 11 6
+
 (c) i = 7; j = 8;
     printf("%d ", i++ - --j);
     printf("%d %d", i, j);
 
+    0 1 7
+
 (d) i = 3; j = 4; k = 5;
     printf("%d ", i++ - j++ + --k);
     printf("%d %d %d", i, j, k);
+
+    3 4 5 4
 */
 
 #include <stdio.h>
@@ -29,7 +34,21 @@ int main(void)
 {   
     int i = 1;
     printf("%d ", i++ - 1);
-    printf("%d", i);
+    printf("%d\n", i);
+
+    int j = 5;
+    i = 10; j = 5;
+    printf("%d ", i++ - ++j);
+    printf("%d %d\n", i, j);
+
+    i = 7; j = 8;
+    printf("%d ", i++ - --j);
+    printf("%d %d\n", i, j);
+
+    int k;
+    i = 3; j = 4; k = 5;
+    printf("%d ", i++ - j++ + --k);
+    printf("%d %d %d\n", i, j, k);
 
     return EXIT_SUCCESS;
 }
