53decd3ef26abcb067401ab55fc7a991e00efd23
JamieSandell <jamie.sandell@outlook.com>
2022-06-04 21:25:11 +0100

    Chapter 4 - Exercise 14

---
+++ Chapter 04/Exercises/14/main.c
@@ -0,0 +1,16 @@
+/*
+Supply parantheses to show how a C compiler would interpret each of the following
+expressions:
+
+(a) a * b - c * d + e
+    (a* b) - (c * d) + e
+
+(b) a / b % c / d
+    ((a / b) % c) / d
+
+(c) - a - b + c - + d
+    (- a) - b + c - (+ d)
+
+(d) a * - b / c - d
+    (((a * (- b)) / c) - d)
+*/
