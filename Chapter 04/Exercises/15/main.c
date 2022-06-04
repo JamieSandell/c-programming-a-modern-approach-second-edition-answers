1f91718bf5066d78fba1c7e5fe1791815fd06511
JamieSandell <jamie.sandell@outlook.com>
2022-06-04 22:02:01 +0100

    Chapter 4 - Exercise 15

---
+++ Chapter 04/Exercises/15/main.c
@@ -0,0 +1,16 @@
+/*
+Give the values of i and j after each of the following expression statements has been
+executed. (Assume that i has value 1 initially and j has the value 2.)
+
+(a) i += j;
+    i = 3, j = 2
+
+(b) i--;
+    i = 0, j = 2
+
+(c) i * j / i;
+    i = 1, j = 2;
+
+(d) i % ++j;
+    i = 1; j = 2
+*/
