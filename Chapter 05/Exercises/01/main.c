c4620fce976927b09957a461af61d3536f78370d
JamieSandell <jamie.sandell@outlook.com>
2022-06-25 07:42:04 +0100

    Chapter 5 - Exercise 1

--- Chapter 05/Exercises/01/main.c
+++ Chapter 05/Exercises/01/main.c
@@ -32,5 +32,33 @@ output produced by each, assuming that i, j, and k are int variables.
     printf("%d", i % j + i < k);
 
     ((i % j) + i) < k;
-    (())
+    ((0) + 3) < 5
+    3 < 5
+    0
+    prints 0
 */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+    int i = 3;
+    int j = 4;
+    int k = 5;
+
+    i = 2; j = 3;
+    k = i * j == 6;
+    printf("%d\n", k);
+
+    i = 5; j = 10; k = 1;
+    printf("%d\n", k > i < j);
+
+    i = 3; j = 2; k = 1;
+    printf("%d\n", i < j == j < k);
+
+    i = 3; j = 4; k = 5;
+    printf("%d", i % j + i < k);
+
+    return EXIT_SUCCESS;
+}
