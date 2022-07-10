0485106c89cd2446b978ac59afa37ae7ac117ab2
jamiesandell <jamie.sandell@outlook.com>
2022-07-10 07:19:55 +0100

    Chapter 5 - Exercise 11

---
+++ Chapter 05/Exercises/11/main.c
@@ -0,0 +1,34 @@
+/*
+The following table shows telephone area codes in the state of Georgia along with the larg-
+est city in each area:
+
+Area Code   Major city
+    229     Albany
+    404     Atlanta
+    470     Atlanta
+    478     Macon
+    678     Atlanta
+    706     Columbus
+    762     Columbus
+    770     Atlanta
+    912     Savannah
+
+Write a switch statement whose controlling expression is the variable area_code. If the
+value of area_code is in the table, the switch will print the corresponding
+city name. Otherwise, the switch statement will display the message "Area code not
+recognized". Use the techniques discussed in 5.3 to make the switch state-
+ment as simple as possible.
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+    int area_code = 229;
+
+    switch (area_code)
+    {
+        
+    }
+}
