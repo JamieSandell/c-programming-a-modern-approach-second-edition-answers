c9e06801834c4ba0be7248ed2eb32ef50bb29a17
Jamie Sandell <Jamie.Sandell@outlook.com>
2022-07-26 12:32:18 +0100

    Chapter 5 - Project 3

--- Chapter 05/Projects/03/main.c
+++ Chapter 05/Projects/03/main.c
@@ -12,42 +12,59 @@ play the rival's commission as well as the commission charged by the original br
 
 int main(void)
 {
-    float commission, value;
+    float commission;
+    int number_of_shares;
+    float price_per_share;
 
-    printf("Enter the value of trade: ");
-    scanf("%f", &value);
+    printf("Enter the number of shares and the price per share: ");
+    scanf("%d %f", &number_of_shares, &price_per_share);
+    float value = number_of_shares * price_per_share;
 
-    if (value < 2500.00f)
+    float rival_broker_commission;
+    if (value < 2000.00f)
     {
         commission = 30.0f + .017f * value;
+        rival_broker_commission = 33.0f + 0.03f * value;
+    }
+    else if (value < 2500.00f)
+    {
+        commission = 30.0f + .017f * value;
+        rival_broker_commission = 33.0f + 0.02f * value;
     }
     else if (value < 6250.00f)
     {
         commission = 56.00f + .0066f * value;
+        rival_broker_commission = 33.0f + 0.02f * value;
     }
     else if (value < 20000.00f)
     {
         commission = 76.00f + .0034 * value;
+        rival_broker_commission = 33.0f + 0.02f * value;
     }
     else if (value < 50000.00f)
     {
         commission = 100.00f + .0022f * value;
+        rival_broker_commission = 33.0f + 0.02f * value;
     }
     else if (value < 50000000.00f)
     {
         commission = 155.00f + .0011f * value;
+        rival_broker_commission = 33.0f + 0.02f * value;
     }
     else
     {
         commission = 255.00f + .0009f * value;
+        rival_broker_commission = 33.0f + 0.02f * value;
     }
 
     if (commission < 39.00f)
     {
         commission = 39.00f;
+        rival_broker_commission = 39.00f;
     }
 
     printf("Commission: $%.2f\n", commission);
+    printf("Rival Commission: $%.2f\n", rival_broker_commission);
 
     return EXIT_SUCCESS;
 }
