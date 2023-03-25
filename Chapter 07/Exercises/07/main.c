627421bf52befc0a38c1e65223f99216d99d9f5a
Jamie Sandell <jamie.sandell@outlook.com>
2023-03-25 06:58:22 +0000

    Chapter 7 - Exercise 7

---
+++ Chapter 07/Exercises/07/main.c
@@ -0,0 +1,15 @@
+/*
+For each of the following character escapes, give the equivalent octal escape. (Assume that the character set is ASCII.)
+You may wishto consult Appendix E, which lists the numerical codes for ASCII characters.
+
+(a) \b
+(b) \n
+(c) \r
+(d) \t
+
+Solution
+(a) \10
+(b) \12
+(c) \15
+(d) \11
+*/
