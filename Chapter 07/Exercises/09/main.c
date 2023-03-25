57450833df5935a84b91808183f1d49e422ddd60
Jamie Sandell <jamie.sandell@outlook.com>
2023-03-25 07:00:26 +0000

    Chapter 7 - Exercise 9

---
+++ Chapter 07/Exercises/09/main.c
@@ -0,0 +1,6 @@
+/*
+Suppose that i and j are variables of type int. What is the type of the expression i / j + 'a'?
+
+Solution
+int. The expression automatically promotes the char value 'a' to an int.
+*/
