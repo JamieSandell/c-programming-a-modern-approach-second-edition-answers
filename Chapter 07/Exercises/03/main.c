23d731c2ab49a486cf3c1cd2c3e22f44c99aaf65
Jamie Sandell <jamie.sandell@outlook.com>
2023-03-25 06:51:46 +0000

    Chapter 7 - Exercise 3

---
+++ Chapter 07/Exercises/03/main.c
@@ -0,0 +1,11 @@
+/*
+Which of the following are not legal types in C?
+
+(a) short unsigned int
+(b) short float
+(c) long double
+(d) unsigned long
+
+Solution
+(b) short float is not a legal type. The only available floating point types are float, double and long double.
+*/
