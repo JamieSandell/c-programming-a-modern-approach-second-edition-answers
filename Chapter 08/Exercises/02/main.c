602c4f4767dd7e4647287bfda96741cb89e53977
Jamie Sandell <jamie.sandell@mosaic-fs.co.uk>
2023-04-13 12:59:52 +0100

    Chapter 8 - Exercise 2

---
+++ Chapter 08/Exercises/02/main.c
@@ -0,0 +1,10 @@
+/*
+The Q&A section shows how to use a letter as an array subscript. Describe how to use a
+digit (in character form) as a subscript.
+
+char c = '9';
+
+char digit_count[10];
+
+digit_count[c - '0'] == digit_count[9]; // true
+*/
