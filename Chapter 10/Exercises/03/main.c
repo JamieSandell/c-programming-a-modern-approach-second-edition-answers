b76f24cec695ddfcbc3d65efead139251abbd003
Jamie Sandell <=>
2023-05-03 12:31:22 +0100

    Chapter 10 - Exercise 3

---
+++ Chapter 10/Exercises/03/main.c
@@ -0,0 +1,6 @@
+/*
+Suppose that a program has only one function (main). How many different variables named
+i could this program contain?
+
+1 extern, 1 local to main, and as many blocked/nested blocks within main as you write.
+*/
