1734bfcd66b00b68044309b1db15dd37a6232913
jamiesandell <jamie.sandell@outlook.com>
2023-05-07 06:54:07 +0100

    Chapter 11 - Exercise 3

---
+++ Chapter 11/Exercises/03/main.c
@@ -0,0 +1,25 @@
+/*
+The following function supposedly computes the sum and average of the numbers in the
+array a, which has length n. avg and sum point to variables that the function should mod-
+ify. Unfortunately, the function contains several errors; find and correct them.
+
+void avg_sum(double a[], int n, double *avg, double *sum)
+{
+    int i;
+
+    sum = 0.0;
+    for (i = 0; i < n; i++)
+        sum += a[i];
+    avg = sum / n;
+}
+
+void avg_sum(double a[], int n, double *avg, double *sum)
+{
+    int i;
+
+    *sum = 0.0;
+    for (i = 0; i < n; i++)
+        *sum += a[i];
+    *avg = *sum / n;
+}
+*/
