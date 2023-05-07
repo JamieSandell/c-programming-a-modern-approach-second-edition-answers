72349852211cad5cebc7b377a66ee5116643ffcd
jamiesandell <jamie.sandell@outlook.com>
2023-05-07 07:40:05 +0100

    Chapter 11 - Exercise 6

---
+++ Chapter 11/Exercises/06/main.c
@@ -0,0 +1,24 @@
+/*
+Write the following function:
+
+void find_two_largest(int a[], int n, int *largest,
+                      int *second_largest);
+
+When passed an array a of length n, the function will search a for its largest and second-
+largest elements, storing them in the variables pointed to by largest and
+second-largest respectively.
+
+void find_two_largest(int a[], int n, int *largest, int *second_largest)
+{
+    *largest = a[0];
+    *second_largest = a[0];
+
+    for (int i = 1; i < n, i++)
+    {
+        if (*largest < a[i])
+        {
+            
+        }
+    }
+}
+*/
