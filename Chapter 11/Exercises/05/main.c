792f7b2c369a5a8fe0d0e667788397fa5a34fb5b
jamiesandell <jamie.sandell@outlook.com>
2023-05-07 07:01:17 +0100

    Chapter 11 - Exercise 5

---
+++ Chapter 11/Exercises/05/main.c
@@ -0,0 +1,18 @@
+/*
+Write the following function
+
+void split_time(long total_sec, int *hr, int *min, int *sec);
+
+total_sec is a time represented as the number of seconds since midnight. hr, min and
+sec are pointers to variables in which the function will store the equivalent time in hours
+(0-23), minutes (0-59) and seconds (0-59), respectively.
+
+void split_time(long total_sec, int *hr, int *min, int *sec)
+{
+    *hr = total_sec / 3600;
+    total_sec -=*hr * 3600;
+    *min = total_sec / 60;
+    total_sec -= *min * 60;
+    *sec = total_sec;
+}
+*/
