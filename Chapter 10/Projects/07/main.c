97d9763644ece442c994e61b3273a39dd64d5525
jamiesandell <jamie.sandell@outlook.com>
2023-05-07 06:18:30 +0100

    Chapter 10 - Project 7

--- Chapter 10/Projects/07/main.c
+++ Chapter 10/Projects/07/main.c
@@ -61,15 +61,15 @@ const int segments[10][7] = {{1, 1, 1, 1, 1, 1, 0}, ...};
 #define MAX_DIGITS 10
 
 const int segments[10][7] = {{1, 1, 1, 1, 1, 1, 0}, // 0
-                                    {0, 1, 1, 0, 0, 0, 0}, // 1
-                                    {1, 1, 1, 0, 1, 0, 1}, // 2
-                                    {1, 1, 1, 1, 0, 0, 1}, // 3
-                                    {0, 1, 1, 0, 0, 1, 1}, // 4
-                                    {1, 0, 1, 1, 0, 1, 1}, // 5
-                                    {1, 0, 1, 1, 1, 1, 1}, // 6
-                                    {1, 1, 1, 0, 0, 0, 0}, //7
-                                    {1, 1, 1, 1, 1, 1, 1}, // 8
-                                    {1, 1, 1, 1, 0, 1, 1}}; // 9
+                            {0, 1, 1, 0, 0, 0, 0}, // 1
+                            {1, 1, 0, 1, 1, 0, 1}, // 2
+                            {1, 1, 1, 1, 0, 0, 1}, // 3
+                            {0, 1, 1, 0, 0, 1, 1}, // 4
+                            {1, 0, 1, 1, 0, 1, 1}, // 5
+                            {1, 0, 1, 1, 1, 1, 1}, // 6
+                            {1, 1, 1, 0, 0, 0, 0}, //7
+                            {1, 1, 1, 1, 1, 1, 1}, // 8
+                            {1, 1, 1, 1, 0, 1, 1}}; // 9
 
 char digits[4][MAX_DIGITS * 4];
 
@@ -93,10 +93,11 @@ int main(void)
             continue;
         }
 
-        process_digit(c - '0', digit_count);
+        process_digit(c - '0', digit_count * 4);
         digit_count++;
-    } while (c != '\n' || digit_count < MAX_DIGITS);
+    } while (c != '\n' && digit_count < MAX_DIGITS);
     
+    print_digits_array();
 
     exit(EXIT_SUCCESS);
 }
@@ -148,7 +149,7 @@ void process_digit(int digit, int position)
       {
         if (segments[seg_row][seg_col])
         {
-          digits[3][1+ position] = '-';
+          digits[3][1 + position] = '_';
         }
         break;
       }
@@ -194,5 +195,13 @@ void process_digit(int digit, int position)
 
 void print_digits_array(void)
 {
+  for (int i = 0; i < 4; i++)
+  {
+    for (int j = 0; j < MAX_DIGITS * 4; j++)
+    {
+      printf("%c ", digits[i][j]);
+    }
 
+    printf("\n");
+  }
 }
