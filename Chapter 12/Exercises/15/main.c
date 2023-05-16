d7bac0c525415409e166972f63f63ef5bf22c828
jamiesandell <jamie.sandell@outlook.com>
2023-05-16 11:56:38 +0100

    Chapter 12 - Exercise 15

---
+++ Chapter 12/Exercises/15/main.c
@@ -0,0 +1,9 @@
+/*
+Write a loop that prints all temperature readings stored in row i of the temperatures
+array (see Exercise 14). Use a pointer to visit each element of the row.
+
+for (int *p = temperatures[i]; p < temperatures[i] + HOURS; p++)
+{
+    printf("%d\n", *p);
+}
+*/
