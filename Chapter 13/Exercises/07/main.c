1b18ca068fa3052b95030e67d7eecb8a80535442
jamiesandell <jamie.sandell@outlook.com>
2023-05-19 14:44:32 +0100

    Chapter 13 - Exercise 7

---
+++ Chapter 13/Exercises/07/main.c
@@ -0,0 +1,12 @@
+/*
+Suppose that str is an array of characters. Which one of the following statements is not
+equivalent to the other three?
+
+(a) *str = 0;
+(b) str[0] = '\0';
+(c) strcpy(str, "");
+(d) strcat(str, "");
+
+a, b, and c assign the null character to str.
+d is the odd one out, it appends the null character to the end of str.
+*/
