fb479a5c3b46ac04932eeec2859b2663775a3364
Jamie Sandell <=>
2023-05-23 13:00:30 +0100

    Chapter 13 - Exercise 12

---
+++ Chapter 13/Exercises/12/main.c
@@ -0,0 +1,49 @@
+/*
+Write the following function:
+
+void get_extension(const char *file_name, char *extension);
+
+file_name points to a string containing a file name. The function should store the exten-
+sion on the file name in the string pointed to by extension. For example, if the file name
+is "memo.txt", the function will store "txt" in the string pointed to by extension. If
+the file name doesn't have an extension, the function should store an empty string (a single
+null character) in the string pointed to by extension. Keep the function as simple as pos-
+sible by having it use the strlen and strcpy functions.
+*/
+
+#include <stdlib.h>
+#include <string.h>
+
+#define LENGTH 10
+
+void get_extension(const char *file_name, char *extension);
+
+int main(void)
+{
+    char file_name[] = "memo.txt";
+    char extension[10] = "";
+
+    get_extension(file_name, extension);
+    printf("File name of %s is %s\n", file_name, extension);
+
+    exit(EXIT_SUCCESS);
+}
+
+void get_extension(const char *file_name, char *extension)
+{
+    int length = strlen(file_name);
+    char *p = file_name + length;
+
+    while (*p-- != '.')
+    {
+        if (p = file_name)
+        {
+            strcpy(extension, "");
+            return;
+        }
+    }
+
+    p++;
+
+    strcpy(extension, p);
+}
