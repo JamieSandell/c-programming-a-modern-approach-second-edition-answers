08078bed1d7179ad9b8f088e95e45e491866511e
Jamie Sandell <=>
2023-06-06 12:29:23 +0100

    Chapter 13 - Project 2

---
+++ Chapter 13/Projects/02/original.c
@@ -0,0 +1,14 @@
+/* Prints a one-month reminder list */
+
+#include <stdio.h>
+#include <string.h>
+
+#define MAX_REMIND 50 /* maximum number of reminders */
+#define MSG_LEN 60 /* max length of reminder message */
+
+int read_line(char str[], int n);
+
+int main(void)
+{
+    char reminders[MAX_REMIND][MSG_LEN+3];
+}
