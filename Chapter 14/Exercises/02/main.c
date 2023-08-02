47072d71d5cee0e4bf422b857b52536a7e1caf61
Jamie Sandell <=>
2023-08-02 16:24:21 +0100

    Chapter 14 - Exercise 2

---
+++ Chapter 14/Exercises/02/main.c
@@ -0,0 +1,6 @@
+/*
+Write a macro NELEMS(a) that computes the number of elements in a one-dimensional
+array a. Hint: See the discussion of sizeof operator in Section 8.1
+
+#define NELEMS(a) (sizeof(a) / sizeof(a)[0])
+*/
