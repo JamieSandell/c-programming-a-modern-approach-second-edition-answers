78e2f95b230194b22a3f89584c89fd5411d80b39
Jamie Sandell <=>
2023-08-09 15:57:11 +0100

    Chapter 14 - Exercise 6

---
+++ Chapter 14/Exercises/06/main.c
@@ -0,0 +1,16 @@
+/*
+(a) Write a macro DISP(f,x) that expands into a call to printf that displays the value
+of the function f when called with argument x. For example,
+
+DISP(sqrt, 3.0);
+
+should expand into
+
+printf("sqrt(%g) = %g\n", 3.0, sqrt(3.0));
+
+(b) Write a macro DISP2(f,x,y) that's similar to DISP but works for functions with
+two arguments.
+
+(a)
+DISP(f,x) (printf("%s(%g) = %g\n", ))
+*/
