3057b26243424d6eff423b8a4bce886b621945d3
Jamie Sandell <=>
2023-09-27 13:35:21 +0100

    Chapter 14 - Exercise 15

---
+++ Chapter 14/Exercises/15/main.c
@@ -0,0 +1,24 @@
+/*
+Suppose that a program needs to display messages in either English, French or Spanish.
+Using a condition compilation, write a program fragment that displays one of the following
+three messages, depending on whether or not the specified macro is defined:
+
+Insert Disk 1       (if ENGLISH is defined)
+Inserez Le Disque 1 (if FRENCH is defined)
+Inserte El Disco 1  (if SPANISH is defined)
+*/
+
+/*
+#if defined(ENGLISH)
+#define INSERT_DISK "Insert Disk 1"
+#
+#elif defined(FRENCH)
+#define INSERT_DISK "Inserez Le Disque 1"
+#
+#else
+#define INSERT_DISK "Inserte El Disco 1"
+#
+#endif
+
+printf("%s\n", INSERT_DISK);
+*/
