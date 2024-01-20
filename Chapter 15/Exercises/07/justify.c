12d5885c50fa3d615d6ad0dc92525fe9dbf4a924
Jamie Sandell <jamie.sandell@outlook.com>
2024-01-20 22:30:41 +0000

    Chapter 15 - Exercise 7

---
+++ Chapter 15/Exercises/07/justify.c
@@ -0,0 +1,51 @@
+/*
+The justify program of Section 15.3 justifies lines by inserting extra spaces between
+words. The way the write_line function currently works, the words closer to the end of
+a line tend to have slightly wider gaps between them than the words at the beginning. (For
+example, the words closer to the end might have three spaces between them, while the
+words closer to the beginning might be separated by only two spaces.) Improve the program
+by having write_line alternate between putting the larger gaps at the end of the line and
+putting them at the beginning of the line.
+*/
+
+/* Formats a file of text */
+
+#include <stdlib.h>
+#include <string.h>
+#include "line.h"
+#include "word.h"
+
+#define MAX_WORD_LENGTH 20
+
+int main(void)
+{
+    char word[MAX_WORD_LENGTH + 2];
+    int word_len;
+
+    clear_line();
+
+    for (;;)
+    {
+        read_word(word, MAX_WORD_LENGTH + 1);
+        word_len = strlen(word);
+
+        if (word_len == 0)
+        {
+            flush_line();
+            return EXIT_SUCCESS;
+        }
+
+        if (word_len > MAX_WORD_LENGTH)
+        {
+            word[MAX_WORD_LENGTH] = '*';
+        }
+
+        if (word_len + 1 > space_remaining())
+        {
+            write_line();
+            clear_line();
+        }
+
+        add_word(word);
+    }
+}
