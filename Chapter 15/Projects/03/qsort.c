b8f838bc123072342a6b74212624898c7aec406a
Jamie Sandell <jamie.sandell@outlook.com>
2024-01-22 08:03:25 +0000

    Chapter 15 - Project 3

--- Chapter 15/Projects/03/qsort.c
+++ Chapter 15/Projects/03/qsort.c
@@ -1,10 +1,37 @@
 /*
-
+Modify the qsort.c program of Section 9.6 so that the quicksort and split func-
+tions are in a separate file named quicksort.c. Create a header file named quick-
+sort.h that contains prototypes for the two functions and have both qsort.c and
+quicksort.c include this file.
 */
 
+#include <stdio.h>
 #include <stdlib.h>
+#include "quicksort.h"
+
+#define N 10
 
 int main(void)
 {
+    int a[N];
+
+    printf("Enter %d numbers to be sorted: ", N);
+
+    for (int i = 0; i < N; ++i)
+    {
+        scanf("%d", &a[i]);
+    }
+
+    quicksort(a, 0, N - 1);
+
+    printf("In sorted order: ");
+
+    for(int i = 0; i < N; ++i)
+    {
+        printf("%d ", a[i]);
+    }
+
+    printf("\n");
+
     return EXIT_SUCCESS;
 }
---
+++ Chapter 15/Projects/03/quicksort.h
@@ -0,0 +1,3 @@
+void quicksort(int a[], int low, int high);
+
+int split(int a[], int low, int high);
