f503dc8a55f57bcfd77758aa494a821dcaef4ac6
JamieSandell <jamie.sandell@outlook.com>
2024-01-25 21:38:08 +0000

    Chapter 16 - Exercise 14

--- Chapter 16/Exercises/14/main.c
+++ Chapter 16/Exercises/14/main.c
@@ -13,6 +13,7 @@ following operations on a shape structure s passed as an argument:
 
 #define RECTANGLE 0
 #define CIRCLE 1
+#define PI 3.14
 
 struct point
 {
@@ -36,8 +37,61 @@ struct shape {
 int area(const struct shape *s);
 struct shape move(const struct shape *s, int x, int y);
 struct shape scale(const struct shape *s, double c);
+void copy(const struct shape *src, struct shape *dest);
 
 int main(void)
 {
     return EXIT_SUCCESS;
+}
+
+int area(const struct shape *s) // (a)
+{
+    if (s->shape_kind == RECTANGLE)
+    {
+        return s->u.rectangle.height * s->u.rectangle.width;
+    }
+    return PI * s->u.circle.radius * s->u.circle.radius;
+}
+
+struct shape move(const struct shape *s, int x, int y) // (b)
+{
+    struct shape result;
+    copy(s, &result);
+    result.center.x += x;
+    result.center.y += y;
+    return result;
+}
+
+struct shape scale(const struct shape *s, double c) // (c)
+{
+    struct shape result;
+    copy(s, &result);
+
+    if (result.shape_kind == RECTANGLE)
+    {
+        result.u.rectangle.height *= c;
+        result.u.rectangle.width *= c;
+    }
+    else
+    {
+        result.u.circle.radius *= c;
+    }
+
+    return result;
+}
+
+void copy(const struct shape *src, struct shape *dest)
+{
+    dest->center = src->center;
+    dest->shape_kind = dest->shape_kind;
+
+    if (dest->shape_kind == RECTANGLE)
+    {
+        dest->u.rectangle.height = src->u.rectangle.height;
+        dest->u.rectangle.width = src->u.rectangle.width;
+    }
+    else
+    {
+        dest->u.circle.radius = src->u.circle.radius;
+    }
 }
