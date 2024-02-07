ec1572feede540aa2cf6d3b175f801107df347ac
Jamie Sandell <=>
2024-02-07 12:57:03 +0000

    Chapter 17 - Exercise 14

--- Chapter 17/Exercises/14/main.c
+++ Chapter 17/Exercises/14/main.c
@@ -16,9 +16,32 @@ struct node
 
 // Delete the first node using n
 void delete_from_list(struct node **list, int n);
+void *my_malloc(size_t size);
+void print_list(const struct node *list);
 
 int main(void)
 {
+    struct node *a = my_malloc(sizeof(*a));
+    struct node *b = my_malloc(sizeof(*b));
+    struct node *c = my_malloc(sizeof(*c));
+
+    a->value = 1;
+    a->next = b;
+    b->value = 10;
+    b->next = c;
+    c->value = 20;
+    c->next = NULL;
+
+    print_list(a);
+
+    struct node **list = &a;
+    delete_from_list(list, 10);
+    print_list(a);
+    delete_from_list(list, 1);
+    print_list(a);
+    delete_from_list(list, 20);
+    print_list(a);
+
     return EXIT_SUCCESS;
 }
 
@@ -41,5 +64,26 @@ void delete_from_list(struct node **list, int n)
     {
         *list = node->next;
         free(node);
+    }
+}
+
+void *my_malloc(size_t size)
+{
+    void *result = malloc(size);
+
+    if (result == NULL)
+    {
+        printf("Error! Couldn't allocate memory\n");
+        exit(EXIT_FAILURE);
+    }
+
+    return result;
+}
+
+void print_list(const struct node *list)
+{
+    for (const struct node *node = list; node != NULL; node = node->next)
+    {
+        printf("%d\n", node->value);
     }
 }
