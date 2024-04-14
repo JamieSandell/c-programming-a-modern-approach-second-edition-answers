f88349c329f76687577592d4add22be21de0a239
Jamie Sandell <jamie.sandell@outlook.com>
2024-04-14 22:51:24 +0100

    Chapter 19 - Exercise 3

--- Chapter 19/Exercises/03/array_queue.c
+++ Chapter 19/Exercises/03/array_queue.c
@@ -3,7 +3,7 @@
 three integers to keep track of the queue's status, with one integer storing the position of the
 first empty slot in the array (used when an item is inserted), the second storing the position
 of the next item to be removed, and the third storing the number of items in the queue.
-An insertion or removal that would caused either of the first two integers to be incremented past
+An insertion or removal that would cause either of the first two integers to be incremented past
 the end of the array should instead reset the variable to zero, thus causing it to "wrap
 around" to the beginning of the array.
 */
@@ -49,12 +49,55 @@ Queue create(int size)
     q->next_item_to_be_removed = 0;
     q->count = 0;
     q->size = size;
+
+    return q;
 }
 
 void destroy(Queue q)
 {
     free(q->contents);
     free(q);
+}
+
+void enqueue(Queue q, Item i)
+{
+    if (q->count >= q->size)
+    {
+        q->first_empty_slot = 0;
+        q->next_item_to_be_removed = 0;
+    }
+
+    q->contents[q->first_empty_slot++] = i;
+    q->count++;
+}
+
+Item dequeue(Queue q)
+{
+    Item i = q->contents[q->next_item_to_be_removed--];
+    q->count--;
+
+    if (q->count < 0)
+    {
+        q->first_empty_slot = 0;
+        q->next_item_to_be_removed = 0;
+    }
+
+    return i;
+}
+
+Item get_first(Queue q)
+{
+    return q->contents[q->next_item_to_be_removed];
+}
+
+Item get_last(Queue q)
+{
+    return q->contents[q->count - 1];
+}
+
+bool is_empty(Queue q)
+{
+    return (q->count == 0);
 }
 
 static void terminate(const char *message)
