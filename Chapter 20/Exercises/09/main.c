6fcbb2f689c924cd64e09d3a07df92ba71615016
Jamie Sandell <jamie.sandell@outlook.com>
2024-04-27 07:28:50 +0100

    Chapter 20 - Exercise 9

---
+++ Chapter 20/Exercises/09/main.c
@@ -0,0 +1,16 @@
+/*
+(a) Write the following function:
+
+int count_ones(unsigned char ch);
+count_ones should return the number of 1 bits in ch.
+
+(b) Write the function in part (a) without using a loop.
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+    return EXIT_SUCCESS;
+}
