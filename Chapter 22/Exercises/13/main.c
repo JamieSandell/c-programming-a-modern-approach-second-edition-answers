f0285fb56f20c356ef3d0a96b32bec6693045096
Jamie Sandell <jamie.sandell@outlook.com>
2024-05-07 07:02:52 +0100

    Chapter 22 - Exercise 13

---
+++ Chapter 22/Exercises/13/main.c
@@ -0,0 +1,24 @@
+/*
+Write the following function:
+
+int line_length(const char *filename, int n);
+
+The function should return the length of line n in the text file whose name is filename
+(assuming that the first line in the file is line 1). If the line doesn't exist, the function should
+return 0.
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int line_length(const char *filename, int n);
+
+int main(void)
+{
+    return EXIT_SUCCESS;
+}
+
+int line_length(const char *filename, int n)
+{
+    int line_count = 0;
+}
