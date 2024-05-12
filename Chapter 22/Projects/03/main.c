cb0b82612eadd4f91a98bcecbc7350e4ad6f3502
Jamie Sandell <jamie.sandell@outlook.com>
2024-05-12 07:05:06 +0100

    Chapter 22 - Project 3

--- Chapter 22/Projects/03/main.c
+++ Chapter 22/Projects/03/main.c
@@ -31,6 +31,35 @@ int main(int argc, char *argv[])
   {
     snprintf(message, MAX_MESSAGE_SIZE, "Error in main: failed to open %s for reading.\n", argv[i]);
     terminate((fp = fopen(argv[i], "r")) == NULL, message);
+
+    snprintf(message, MAX_MESSAGE_SIZE, "Error in main: failed to seek to end of %s\n", argv[i]);
+    terminate((fseek(fp, 0L, SEEK_END) != 0), message);
+
+    snprintf(message, MAX_MESSAGE_SIZE, "Error in main: failed to get the size of %s\n", argv[i]);
+    terminate((file_size = ftell(fp)) == -1, message);
+
+    snprintf(message, MAX_MESSAGE_SIZE, "Error in main: failed to seek to beginning of %s\n", argv[i]);
+    terminate((fseek(fp, 0L, SEEK_SET) != 0), message);
+
+    snprintf(message, MAX_MESSAGE_SIZE, "Error in main: failed to allocate memory to read the contents of %s\n", argv[i]);
+    terminate(((file_contents = malloc(file_size + 1)) == NULL), message);
+
+    size_t result = fread(file_contents, 1, file_size, fp);
+
+    if (result == (size_t)file_size)
+    {
+      file_contents[result] = '\0';
+      fprintf(stdout, "%s", file_contents);
+    }
+
+    snprintf(message, MAX_MESSAGE_SIZE, "Error in main: unexpected EOF whilst reading %s\n", argv[i]);
+    terminate(feof(fp), message);
+    snprintf(message, MAX_MESSAGE_SIZE, "Error in main: error whilst reading %s\n", argv[i]);
+    terminate(ferror(fp), message);
+
+    free(file_contents);
+    snprintf(message, MAX_MESSAGE_SIZE, "Error in main: failed to properly close %s\n", argv[i]);
+    terminate(fclose(fp) == EOF, message);
   }
 
   return EXIT_SUCCESS;
