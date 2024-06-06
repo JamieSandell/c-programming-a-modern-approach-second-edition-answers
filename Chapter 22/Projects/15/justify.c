9998047ad4857e74207f9762268d1c5caa791d6d
Jamie Sandell <jamie.sandell@mosaic-fs.co.uk>
2024-06-06 16:10:58 +0100

    Chapter 22 - Project 15

--- Chapter 22/Projects/15/justify.c
+++ Chapter 22/Projects/15/justify.c
@@ -19,28 +19,40 @@ to another. Have the program obtain the names of both files from the command lin
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "line.h"
-#include "word.h"
 
 #define MAX_MESSAGE_LENGTH 512
+#define MAX_LINE_LEN 60
 #define MAX_WORD_LEN 20
 
+FILE *fpr;
+FILE *fpw;
+char line[MAX_LINE_LEN+1];
+int line_len = 0;
+int num_words = 0;
+char message[MAX_MESSAGE_LENGTH];
+
+void clear_line(void);
+void add_word(const char *word);
+int space_remaining(void);
+void write_line(void);
+void flush_line(void);
+int read_char(void);
+void read_word(char *word, int len);
 void terminate(bool condition, const char *message);
 
 int main(int argc, char *argv[])
 {
-  char message[MAX_MESSAGE_LENGTH];
   snprintf(message, MAX_MESSAGE_LENGTH, "Error: Incorrect usage. Expected usage: justify input.txt output.txt\n");
   terminate(argc != 3, message);
 
   char word[MAX_WORD_LEN+2];
   int word_len;
   
-  FILE *fpr = fopen(argv[1], "r");
+  fpr = fopen(argv[1], "r");
   snprintf(message, MAX_MESSAGE_LENGTH, "Error: failed to open %s for reading.\n", argv[1]);
   terminate(fpr == NULL, message);
 
-  FILE *fpw = fopen(argv[2], "w");
+  fpw = fopen(argv[2], "w");
   snprintf(message, MAX_MESSAGE_LENGTH, "Error: failed to open %s for writing.\n", argv[2]);
   terminate(fpw == NULL, message);
 
@@ -62,6 +74,81 @@ int main(int argc, char *argv[])
   }
 
   return EXIT_SUCCESS;
+}
+
+int read_char(void)
+{
+  int ch = fgetc(fpr);
+
+  if (ch == '\n' || ch == '\t')
+    return ' ';
+  return ch;
+}
+
+void read_word(char *word, int len)
+{
+  int ch, pos = 0;
+
+  while ((ch = read_char()) == ' ')
+    ;
+  while (ch != ' ' && ch != EOF) {
+    if (pos < len)
+      word[pos++] = ch;
+    ch = read_char();
+  }
+  word[pos] = '\0';
+}
+
+void clear_line(void)
+{
+  line[0] = '\0';
+  line_len = 0;
+  num_words = 0;
+}
+
+void add_word(const char *word)
+{
+  if (num_words > 0) {
+    line[line_len] = ' ';
+    line[line_len+1] = '\0';
+    line_len++;
+  }
+  strcat(line, word);
+  line_len += strlen(word);
+  num_words++;
+}
+
+int space_remaining(void)
+{
+  return MAX_LINE_LEN - line_len;
+}
+
+void write_line(void)
+{
+  int extra_spaces, spaces_to_insert, i, j;
+
+  extra_spaces = MAX_LINE_LEN - line_len;
+  snprintf(message, MAX_MESSAGE_LENGTH, "Error: Failed writing to file.\n");
+  for (i = 0; i < line_len; i++) {
+    if (line[i] != ' ')
+    {
+      terminate(fputc(line[i], fpw) == EOF, message);
+    }
+    else {
+      spaces_to_insert = extra_spaces / (num_words - 1);
+      for (j = 1; j <= spaces_to_insert + 1; j++)
+        terminate(fputc(' ', fpw) == EOF, message);
+      extra_spaces -= spaces_to_insert;
+      num_words--;
+    }
+  }
+  terminate(fputc('\n', fpw) == EOF, message);
+}
+
+void flush_line(void)
+{
+  if (line_len > 0)
+    fputs(line, fpw);
 }
 
 void terminate(bool condition, const char *message)
