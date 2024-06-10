4721dc4821eb10da355b9a03556ca37c6e022dc1
Jamie Sandell <jamie.sandell@mosaic-fs.co.uk>
2024-06-10 16:36:33 +0100

    Chapter 23 - Project 7

---
+++ Chapter 23/Projects/07/main.c
@@ -0,0 +1,19 @@
+/*
+Write a program that counts the number of sentences in a text file (obtained from standard
+input). Assume that each sentence ends with a ., ? or ! followed by a white-space charac-
+ter (including \n).
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(void)
+{
+    int c;
+    int prev;
+
+    
+
+    return EXIT_SUCCESS;
+}
