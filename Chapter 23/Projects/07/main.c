5c87522966123346d022c3afa91f1de426a34bc1
JamieSandell <jamie.sandell@outlook.com>
2024-06-10 21:43:45 +0100

    Chapter 23 - Project 7

--- Chapter 23/Projects/07/main.c
+++ Chapter 23/Projects/07/main.c
@@ -4,16 +4,47 @@ input). Assume that each sentence ends with a ., ? or ! followed by a white-spac
 ter (including \n).
 */
 
+#include <ctype.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
+#define MAX_LINE_LENGTH 1024
+
 int main(void)
 {
-    int c;
-    int prev;
+    int sentence_count = 0;
+    char line[MAX_LINE_LENGTH];
+    char *p;
 
+    while (*(fgets(line, MAX_LINE_LENGTH, stdin)) != EOF)
+    {
+        p = line;
+
+        while (*p != '\0')
+        {
+            if (*p == '.' || *p == '?' || *p == '!')
+            {
+                ++p;
+
+                if (isspace(*p) || *p == '\0')
+                {
+                    ++sentence_count;
+
+                    if (*p != '\0')
+                    {
+                        ++p;
+                    }
+                }
+
+                continue;
+            }
+
+            ++p;
+        }        
+    }
     
+    printf("Found %d sentences.\n", sentence_count);
 
     return EXIT_SUCCESS;
 }
